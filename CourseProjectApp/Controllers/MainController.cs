a925a08004759b04cc4a01fdbf4848c7c8a0866f
Bakshinder <bakshinder@live.com>
2018-05-20 23:25:59 +0530

    first commit

---
+++ CourseProjectApp/Controllers/MainController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProjectApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProjectApp.Controllers
+{
+   // [Route("[Controller]")]
+    public class MainController : Controller
+    {
+        //[Route("[Action]")]
+        public IActionResult Index()
+        {
+            //return new JsonResult("{bakshinder:sethi}");
+            ViewBag.value = "My First MVC Rout!!";
+            return View();
+        }
+
+        //[Route("[Action]")]
+        public IActionResult FirstView()
+        {
+            var model = new MyData { MyId = 5, MyValue = "My View" };
+            return View(model);
+        }
+
+        //[Route("[Action]")]
+        public ContentResult IdRoute()
+        {
+            return Content("Attribuet");
+        }
+
+        //[Route("[Action]")]
+        public ObjectResult MyObject()
+        {
+            var mymodel = new MyData { MyId=1,MyValue="MyFirstValue"};
+            return new ObjectResult(mymodel);
+        } 
+    }
+}
---
+++ CourseProjectApp/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseProjectApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Main/Error");
+            }
+            app.UseStaticFiles();
+            //app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Main}/{action=Index}/{id?}");
+            //});
+            app.UseMvc(ConfigurationRoute);
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void ConfigurationRoute(IRouteBuilder obj)
+        {
+            obj.MapRoute("Default", "{controller=Main}/{action=Index}/{id?}");
+        }
+    }
+}
